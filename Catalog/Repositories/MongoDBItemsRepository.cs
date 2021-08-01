8f212132c4700bc0331eb377323de7039f2a98a8
LeiscarT <leiscartp29@gmail.com>
2021-08-01 02:21:29 -0400

    Crud with mongoDB

--- Catalog/Repositories/MongoDBItemsRepository.cs
+++ Catalog/Repositories/MongoDBItemsRepository.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using Catalog.Entities;
+using MongoDB.Bson;
 using MongoDB.Driver;
 
 namespace Catalog.Repositories
@@ -11,6 +12,7 @@ namespace Catalog.Repositories
         private const string collectionName ="items";
 
         private readonly IMongoCollection<Item> itemsCollection;
+        private readonly FilterDefinitionBuilder<Item> filterBuilder = Builders<Item>.Filter;
 
         public MongoDbItemsRepository(IMongoClient mongoClient){
             IMongoDatabase database = mongoClient.GetDatabase(databaseName);
@@ -23,22 +25,25 @@ namespace Catalog.Repositories
 
         public void DeleteItem(Guid id)
         {
-            throw new NotImplementedException();
+            var filter = filterBuilder.Eq(item => item.Id, id);
+            itemsCollection.DeleteOne(filter);
         }
 
         public Item GetItem(Guid id)
         {
-            throw new NotImplementedException();
+            var filter = filterBuilder.Eq(item => item.Id, id);
+            return itemsCollection.Find(filter).SingleOrDefault();
         }
 
         public IEnumerable<Item> GetItems()
         {
-            throw new NotImplementedException();
+            return itemsCollection.Find(new BsonDocument()).ToList(); //busca todos los documentos y los lista.
         }
 
         public void UpdateItem(Item item)
         {
-            throw new NotImplementedException();
+           var filter = filterBuilder.Eq(existingItem => existingItem.Id, item.Id);
+           itemsCollection.ReplaceOne(filter,item);
         }
     }
 }
