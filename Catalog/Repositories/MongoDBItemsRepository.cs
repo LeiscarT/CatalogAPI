0ff45db7a10c8048eb6e10eece3fe1d432dacc71
LeiscarT <leiscartp29@gmail.com>
2021-08-01 01:31:56 -0400

    Configure port and first step to the docker image

--- Catalog/Repositories/MongoDBItemsRepository.cs
+++ Catalog/Repositories/MongoDBItemsRepository.cs
@@ -18,7 +18,7 @@ namespace Catalog.Repositories
         }
         public void CreateItem(Item item)
         {
-            throw new NotImplementedException();
+           itemsCollection.InsertOne(item);
         }
 
         public void DeleteItem(Guid id)
